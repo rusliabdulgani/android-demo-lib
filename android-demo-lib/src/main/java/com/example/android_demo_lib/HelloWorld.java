b6602f3d7d64338b439285fae9d2e2ca4f18ae30
rusli abdul gani <abdul.gani@fullertonhealth.com>
2024-10-29 08:35:04 +0700

    ganti project

---
+++ android-demo-lib/src/main/java/com/example/android_demo_lib/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.android_demo_lib;
+
+import android.util.Log;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printing(String tag, String message) {
+        Log.d(tag, message);
+    }
+}
